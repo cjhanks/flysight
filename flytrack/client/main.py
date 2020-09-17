4612e4e1eb7c2e24d7fc2b540a85b19f48840066
cjhanks <web@cjhanks.name>
2020-09-16 23:03:27 -0700

    Slightly better style.

--- flytrack/client/main.py
+++ flytrack/client/main.py
@@ -5,12 +5,7 @@ from matplotlib import cm
 from PySide2 import (
         QtWidgets,
         QtCore,
-        QtGui
-        #QApplication,
-        #QDialog,
-        #QLineEdit,
-        #QPushButton,
-        #QWidget,
+        QtGui,
         )
 from flytrack.client import Client
 
@@ -18,14 +13,17 @@ from flytrack.client import Client
 class MainDisplay(QtWidgets.QMainWindow):
     def __init__(self, args):
         super().__init__()
+        #self.setStyleSheet('''
+        #background: rgb(246, 124, 37);
+        #''')
+
         self.reader = cv2.VideoCapture(args.video)
         self.client = Client('tcp://127.0.0.1:8003')
 
         self.frame_count  = self.reader.get(cv2.CAP_PROP_FRAME_COUNT)
         self.frame_width  = self.reader.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.frame_height = self.reader.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        #self.resize(self.frame_width, self.frame_height)
-
+        self.resize(self.frame_width - 256, self.frame_height)
         self.setup_ui()
 
     def setup_ui(self):
@@ -39,20 +37,72 @@ class MainDisplay(QtWidgets.QMainWindow):
         self.slider.setOrientation(QtCore.Qt.Horizontal)
         self.slider.setMaximum(self.frame_count)
         self.slider.setSizePolicy(
-                QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum,
                                       QtWidgets.QSizePolicy.Minimum))
+        self.slider.setStyleSheet('''
+QSlider::groove:horizontal {
+border: 1px solid #bbb;
+background: white;
+height: 10px;
+border-radius: 4px;
+}
+
+QSlider::sub-page:horizontal {
+background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1,
+    stop: 0 #f5c39f, stop: 1 #f67c25);
+border: 1px solid #777;
+height: 10px;
+border-radius: 4px;
+}
+
+QSlider::add-page:horizontal {
+background: #fff;
+border: 1px solid #777;
+height: 10px;
+border-radius: 4px;
+}
+
+QSlider::handle:horizontal {
+background: qlineargradient(x1:0, y1:0, x2:1, y2:1,
+    stop:0 #404040, stop:1 #404040);
+border: 1px solid #777;
+width: 13px;
+margin-top: -2px;
+margin-bottom: -2px;
+border-radius: 4px;
+}
+
+QSlider::handle:horizontal:hover {
+background: qlineargradient(x1:0, y1:0, x2:1, y2:1,
+    stop:0 #000000, stop:1 #000000);
+border: 1px solid #444;
+border-radius: 4px;
+}
+''')
         self.layout.addWidget(self.slider, 0, 0, 1, 1)
 
         self.lcd = QtWidgets.QLCDNumber(self)
         self.lcd.setSizePolicy(
-                QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum,
-                                      QtWidgets.QSizePolicy.Minimum))
-        self.layout.addWidget(self.lcd, 0, 1, 1, 1)
+                QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                      QtWidgets.QSizePolicy.Fixed))
+
+        self.lcd.setStyleSheet('''
+QLCDNumber{
+  background-color: #000000;
+  border: 2px solid #404040;
+  border-width: 2px;
+  border-radius: 10px;
+  color: #f67c25;
+}
+''')
+        self.layout.addWidget(self.lcd, 0, 1, 1, 1,
+                              QtCore.Qt.AlignCenter)
 
         self.slider.valueChanged.connect(self.lcd.display)
         self.slider.sliderReleased.connect(self.changed_frame_index)
         # }
 
+        # Add display zoom
         self.zoom = DisplayZoom(self)
         self.zoom.setSizePolicy(
                 QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
@@ -68,25 +118,24 @@ class MainDisplay(QtWidgets.QMainWindow):
         # {
         menubar = self.menuBar()
         options = menubar.addMenu('Options')
-        menu_show_image = \
-                options.addAction('Show Image',
-                                  shortcut=QtGui.QKeySequence('Ctrl+I'))
+        menu_show_image = options.addAction('Show Image')
+        menu_show_image.toggled.connect(self.display.set_show_image)
+        menu_show_image.setShortcut(QtGui.QKeySequence('Ctrl+I'))
         menu_show_image.setCheckable(True)
         menu_show_image.setChecked(True)
 
-        menu_show_peaks = \
-                options.addAction('Show Peaks',
-                                  shortcut=QtGui.QKeySequence('Ctrl+P'))
+        menu_show_peaks = options.addAction('Show Peaks')
+        menu_show_peaks.toggled.connect(self.display.set_show_peaks)
+        menu_show_peaks.setShortcut(QtGui.QKeySequence('Ctrl+P'))
         menu_show_peaks.setCheckable(True)
         menu_show_peaks.setChecked(False)
 
-        menu_show_heatmap = \
-                options.addAction('Show HeatMap',
-                                  shortcut=QtGui.QKeySequence('Ctrl+H'))
+        menu_show_heatmap = options.addAction('Show HeatMap')
+        menu_show_heatmap.toggled.connect(self.display.set_show_heatmap)
+        menu_show_heatmap.setShortcut(QtGui.QKeySequence('Ctrl+H'))
         menu_show_heatmap.setCheckable(True)
         menu_show_heatmap.setChecked(True)
         # }
-
 
     def changed_frame_index(self):
         frame_idx = int(self.lcd.value())
@@ -101,6 +150,7 @@ class MainDisplay(QtWidgets.QMainWindow):
 
     def update_zoom(self, image):
         self.zoom.update_image(image)
+
 
 class DisplayZoom(QtWidgets.QWidget):
     def __init__(self, parent):
@@ -126,6 +176,7 @@ class DisplayZoom(QtWidgets.QWidget):
             print(err)
         painter.end()
 
+
 class DisplayTool(QtWidgets.QWidget):
     def __init__(self, parent, client, width, height):
         super().__init__(parent)
@@ -133,6 +184,12 @@ class DisplayTool(QtWidgets.QWidget):
         self.client = client
         self.resize(width, height)
         self.setup_ui()
+
+        # {
+        self.show_image = False
+        self.show_peaks= False
+        self.show_heatmap = False
+        # }
 
         # {
         self.frame_idx = -1
@@ -154,9 +211,22 @@ class DisplayTool(QtWidgets.QWidget):
         self.setSizePolicy(policy)
         self.setMouseTracking(True)
 
-    @QtCore.Slot('set_show_image')
-    def set_show_image(self):
-        print('HERE')
+        pal = self.palette()
+        pal.setColor(QtGui.QPalette.Background, QtCore.Qt.black)
+        self.setAutoFillBackground(True)
+        self.setPalette(pal)
+
+    def set_show_image(self, doit):
+        self.show_image = doit
+        self.update()
+
+    def set_show_peaks(self, doit):
+        self.show_peaks = doit
+        self.update()
+
+    def set_show_heatmap(self, doit):
+        self.show_heatmap = doit
+        self.update()
 
     def load_image(self, frame_idx, image):
         self.image = image[:, :, 0].copy()
@@ -193,6 +263,9 @@ class DisplayTool(QtWidgets.QWidget):
         painter.end()
 
     def __draw_base(self, painter):
+        if not self.show_image:
+            return
+
         img = QtGui.QImage(self.image,
                            self.image.shape[0],
                            self.image.shape[1],
@@ -201,6 +274,9 @@ class DisplayTool(QtWidgets.QWidget):
         painter.drawImage(0, 0, self.qimage)
 
     def __draw_heatmap(self, painter):
+        if not self.show_heatmap:
+            return
+
         # Normalize
         min = np.min(self.heatmap)
         max = np.max(self.heatmap)
@@ -210,7 +286,10 @@ class DisplayTool(QtWidgets.QWidget):
         heatmap8 = (heatmap * 255).astype(np.uint8)
         indices = np.linspace(0, 1, 256)
         rgbas = cm.jet(indices)
-        rgbas[:,3] *= indices
+
+        # TODO: Document
+        if self.show_image:
+            rgbas[:,3] *= indices
 
         rgbas = [QtGui.QColor(
                     int(r * 255),
@@ -228,10 +307,14 @@ class DisplayTool(QtWidgets.QWidget):
         painter.drawImage(0, 0, self.qheat_map)
 
     def __draw_peaks(self, painter):
-        painter.setPen(QtCore.Qt.red)
+        if not self.show_peaks:
+            return
+
+        painter.setPen(QtGui.QColor('#f67c25'))
+        mindim = min(self.size().height(), self.size().width())
         for (r, c) in self.peaks:
-            row = self.qimage.size().height() * r
-            col = self.qimage.size().width() * c
+            row = mindim * r
+            col = mindim * c
             center = QtCore.QPoint(row, col)
             origin = QtCore.QPoint(row, col - 20)
             lhs    = QtCore.QPoint(row - 5, col - 7)
@@ -240,7 +323,6 @@ class DisplayTool(QtWidgets.QWidget):
             painter.drawLine(center, origin)
             painter.drawLine(center, lhs)
             painter.drawLine(center, rhs)
-
 
 
 def main(argv=None):
