3dc0819df8897d9edfa056657c5116bf81bfd7d0
cjhanks <web@cjhanks.name>
2020-09-18 13:39:00 -0700

    Add data fetcher.

---
+++ data/get.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+wget -O small.mp4 "https://www.dropbox.com/s/b8974dstznzjzv4/test_clip.15s.mp4?dl=1"
+wget -O large.mp4 "https://www.dropbox.com/s/r6bounkedemplz2/many_flies.clip.mp4?dl=1"
+
