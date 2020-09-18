5160c7716ecaaf295c4b8ee8c932a0086711aee7
cjhanks <web@cjhanks.name>
2020-09-18 14:38:42 -0700

    Add missing setup.py.

---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+setuptools.setup(
+    name='flytrack',
+    version="0.0.1",
+    author='CjHanks',
+    author_email="web@cjhanks.name",
+    description="Fly Tracker visualizer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'matplotlib==3.3.0',
+        'numpy==1.18.5',
+        'opencv-python==4.4.0.42',
+        'protobuf==3.12.4',
+        'pyside2==5.14.0',
+        'pyyaml==5.3.1',
+        'pyzmq==19.0.2',
+        'tensorflow==2.3.0',
+    ],
+    entry_points = {
+        'console_scripts': ['flytrack=flytrack.run:main'],
+    },
+    data_files=[
+
+        ('flytrack', [
+            'flytrack/config.yml'
+        ]),
+        ('flytrack/client/resource',    [
+            'flytrack/client/resource/lcd.css',
+            'flytrack/client/resource/slider.css'
+        ]),
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+)
